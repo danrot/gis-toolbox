c50c2abbfc4b47dc5025b46924037f0328f7289b
Daniel Rotter <daniel.rotter@gmail.com>
2014-11-20 20:24:16 +0100

    added distanceTo method to Point

--- src/at/fhv/gis/shapes/Point.java
+++ src/at/fhv/gis/shapes/Point.java
@@ -28,4 +28,19 @@ public class Point {
     public void setLongitude(double longitude) {
         this.longitude = longitude;
     }
+
+    public double distanceTo(Point point)
+    {
+        double latitude1 = Math.toRadians(this.latitude);
+        double latitude2 = Math.toRadians(point.latitude);
+        double longitude1 = Math.toRadians(this.longitude);
+        double longitude2 = Math.toRadians(point.longitude);
+
+        double result = Math.acos(
+                Math.sin(latitude1)*Math.sin(latitude2) +
+                        Math.cos(latitude1)*Math.cos(latitude2)*Math.cos(longitude1 - longitude2)
+        );
+
+        return result / (2 * Math.PI) * 40007863;
+    }
 }
--- tests/at/fhv/gis/shapes/PointTest.java
+++ tests/at/fhv/gis/shapes/PointTest.java
@@ -6,5 +6,12 @@ import static org.junit.Assert.*;
 
 public class PointTest
 {
+    @Test
+    public void distanceTo()
+    {
+        Point dornbirn = new Point(47.413417, 9.744417);
+        Point feldkirch = new Point(47.238056, 9.598333);
 
+        assertEquals(22410, dornbirn.distanceTo(feldkirch), 100);
+    }
 }
